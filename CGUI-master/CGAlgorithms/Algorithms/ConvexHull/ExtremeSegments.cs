8a8741fb5645a4fe556575e6070ec619ba9d08e5
Abdelrahman Sayed Ali <68053788+abdelrhman2023@users.noreply.github.com>
2022-12-02 02:26:27 +0200

    extreme segments and graham algorithms

--- CGUI-master/CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
+++ CGUI-master/CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
@@ -12,6 +12,93 @@ namespace CGAlgorithms.Algorithms.ConvexHull
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
 
+            int connectPoint = -1 , endpoint=-1;
+
+            if (points.Count < 4)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    outPoints.Add(points[i]);
+                }
+                return;
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                int F = 0;
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (points[i].Equals(points[j])) continue;
+
+                    Line l = new Line(points[i], points[j]);
+                    var flagF = 0;
+                    var flagT = 0;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (points[k].Equals(points[i]) || points[k].Equals(points[j])) continue;
+
+                        if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Right)
+                        {
+                            flagT = 1;
+                        }
+                        if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Left)
+                        {
+                            flagF = 1;
+                        }
+
+                        if (flagT == flagF) break;
+                    }
+
+                    if (flagF != flagT)
+                    {
+                        endpoint = j;
+                        connectPoint = i;
+                        outPoints.Add(points[j]);
+                        outPoints.Add(points[i]);
+                        F = 1;
+                        break;
+                    }
+                }
+                if (F==1) break;
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].Equals(points[connectPoint])) continue;
+
+                Line l = new Line(points[connectPoint], points[i]);
+
+                var flagF = 0;
+                var flagT = 0;
+                
+                for (int k = 0; k < points.Count; k++)
+                {
+                    if (points[k].Equals(points[i]) || points[k].Equals(points[connectPoint])) continue;
+                   
+                    if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Right)
+                    {
+                        flagT = 1;
+                    }
+                    if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Left)
+                    {
+                        flagF = 1;
+                    }
+
+                    if (flagT == flagF) break;
+
+                }
+
+                if (flagF != flagT)
+                {
+                    if (!outPoints.Contains(points[i]))
+                    {
+                        connectPoint = i;
+                        outPoints.Add(points[i]);
+                        i = 0;
+                    }
+                }
+            }
+
         }
 
         public override string ToString()
@@ -20,3 +107,5 @@ namespace CGAlgorithms.Algorithms.ConvexHull
         }
     }
 }
+
+
--- CGUI-master/CGAlgorithms/Algorithms/ConvexHull/GrahamScan.cs
+++ CGUI-master/CGAlgorithms/Algorithms/ConvexHull/GrahamScan.cs
@@ -9,8 +9,77 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class GrahamScan : Algorithm
     {
+        double angle(Point p1 , Point p2)
+        {
+            double xDiff = p2.X - p1.X;
+            double yDiff = p2.Y - p1.Y;
+            return Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI;
+        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+
+            if (points.Count < 4)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    outPoints.Add(points[i]);
+                }
+                return;
+            }
+
+            var orderedPoints = points.OrderBy(x => x.Y).ToList();
+
+            Point convex_points_1;
+            Point convex_points_2;
+
+            convex_points_1 = orderedPoints[0];
+            
+            orderedPoints.RemoveAt(0);
+
+            List<KeyValuePair<Point,double>> Angles = new List<KeyValuePair<Point,double>>();
+            
+            for (int i=0; i < orderedPoints.Count; i++)
+            {
+                Angles.Add(new KeyValuePair<Point, double>(orderedPoints[i],angle(convex_points_1, orderedPoints[i])));
+            }
+
+            var orderedangles = Angles.OrderBy(x => x.Value).ToList();
+            convex_points_2 = orderedangles[0].Key;
+            orderedangles.RemoveAt(0);
+
+            outPoints.Add(convex_points_1);
+            outPoints.Add(convex_points_2);
+
+            while (orderedangles.Count > 0)
+            {
+                Line l = new Line(convex_points_1, convex_points_2);
+                Point point = orderedangles[0].Key;
+                outPoints.Add(point);
+                orderedangles.RemoveAt(0);
+
+                if (HelperMethods.CheckTurn(l, point) == Enums.TurnType.Left)
+                {
+                    convex_points_1 = convex_points_2;
+                    convex_points_2 = point;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        convex_points_1 = outPoints[outPoints.Count - 3];
+                        convex_points_2 = outPoints[outPoints.Count - 1];
+                        outPoints.RemoveAt(outPoints.Count - 2);
+
+                        Line ll = new Line(outPoints[outPoints.Count - 3], outPoints[outPoints.Count - 2]);
+                        Point po = outPoints[outPoints.Count - 1];
+                        if (HelperMethods.CheckTurn(ll, po) == Enums.TurnType.Left)
+                        {
+                            break;
+                        }
+                    }
+
+                }
+            }
 
         }
 
